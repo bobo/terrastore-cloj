9e14b8888f955c6917437bb423fe4b6fd97a3a9b
Mikael Sundberg <mikael.sundberg82@gmail.com>
2011-02-22 20:19:18 +0100

    get/put/query/range query now works

--- src/terrastore/terrastore_dsl.clj
+++ src/terrastore/terrastore_dsl.clj
@@ -14,15 +14,22 @@
 (defn- create-get* [bucket key predicate]
   (str   *server* bucket "/" key "?predicate=js:" (URLEncoder/encode predicate)))
 
+(defmacro predicate [query]
+  `{"predicate" (str "js:" (clj-to-js ~query))})
+
 (defmacro get-str [key & {query :when bucket :from}] 
   `(create-get*  ~bucket ~key (clj-to-js~query)))
 
 (defmacro get [key & {query :when bucket :from}] 
-  `(conditionally-get-value *server*  ~bucket ~key {"predicate" (str "js:" (clj-to-js ~query))}))
-
+  `(conditionally-get-value *server* ~bucket ~key (predicate ~query)))
 
 (defmacro put [document & {query :when bucket :into key :with}] 
-  `(conditionally-put-value *server*  ~bucket ~key ~document {"predicate" (str "js:" (clj-to-js ~query))}))
+  `(conditionally-put-value *server*  ~bucket ~key ~document (predicate ~query)))
 
+(defmacro query [bucket & {query :where}]
+  `(do-predicate-query *server* ~bucket (predicate ~query)))
+
+(defmacro range-query [bucket & { from :from to :to query :when}]
+  `(do-range-query *server* ~bucket {"predicate" (str "js:" (clj-to-js ~query)) "startKey" ~from "endKey" ~to}))
 
 
