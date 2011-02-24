290b6108d4bdf9f53bb7378e6d5eff3e5a471a9e
Mikael Sundberg <mikael.sundberg82@gmail.com>
2011-02-24 20:04:31 +0100

    made predicates optimal in get and put, functions now returns map instead of string

--- src/terrastore/terrastore_dsl.clj
+++ src/terrastore/terrastore_dsl.clj
@@ -1,6 +1,7 @@
 (ns terrastore.terrastore-dsl
   (:use terrastore.terrastore-cloj )
   (:use terrastore.terrastore-ops)
+  (:require [clojure.contrib.json :as json])
   (import [java.net URLEncoder]))
 
 (def *server* "http://localhost:8000/")
@@ -20,28 +21,28 @@
     (str more)))
 
 
+(defn as-map [json]
+  (json/read-json json))
 
-(def type-json {"Content-Type" "applucation/json"})
-
-(defn- create-get* [bucket key predicate]
-  (str   *server* bucket "/" key "?predicate=js:" (URLEncoder/encode predicate)))
 
 (defmacro predicate [query]
   `{"predicate" (str "js:" (clj-to-js ~query))})
 
-(defmacro get-str [key & {query :when bucket :from}] 
-  `(create-get*  ~bucket ~key (clj-to-js~query)))
 
-(defmacro get [key & {query :when bucket :from}] 
-  `(conditionally-get-value *server* ~bucket ~key (predicate ~query)))
+(defmacro get [key & {query :when bucket :from}]
+  `(as-map (if ~query
+                     (conditionally-get-value *server* ~bucket ~key (predicate ~query))
+                     (get-value *server* ~bucket ~key))))
 
-(defmacro put [document & {query :when bucket :into key :with}] 
-  `(conditionally-put-value *server*  ~bucket ~key ~document (predicate ~query)))
+(defmacro put [document & {query :when bucket :into key :with}]
+   (if query
+    `(conditionally-put-value *server*  ~bucket ~key ~document (predicate ~query))
+    `(put-value *server* ~bucket ~key ~document)))
 
 (defmacro query [bucket & {query :where}]
-  `(do-predicate-query *server* ~bucket (predicate ~query)))
+  `(as-map (do-predicate-query *server* ~bucket (predicate ~query))))
 
 (defmacro range-query [bucket & { from :from to :to query :when}]
-  `(do-range-query *server* ~bucket {"predicate" (str "js:" (clj-to-js ~query)) "startKey" ~from "endKey" ~to}))
+  `(as-map (do-range-query *server* ~bucket {"predicate" (str "js:" (clj-to-js ~query)) "startKey" ~from "endKey" ~to})))
 
 
